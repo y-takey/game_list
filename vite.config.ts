31ce8126925e37a37ee37fe2bfda910ecd9a9615
takeyuichi <y.takey@gmail.com>
2024-08-11 16:22:58 +0900

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
+
+const basename = "/game_list/";
 
 export default defineConfig({
   plugins: [
@@ -11,7 +15,15 @@ export default defineConfig({
         v3_throwAbortReason: true,
       },
       ssr: false,
+      basename,
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(join(buildPath, "index.html"), join(buildPath, "404.html"));
+      },
     }),
     tsconfigPaths(),
   ],
+  base: basename,
 });
