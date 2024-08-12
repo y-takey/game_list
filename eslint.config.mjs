fc4694be97eb07886fc2dc6055c8fea61376cb68
takeyuichi <y.takey@gmail.com>
2024-08-12 15:14:10 +0900

    auto correct

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,102 +13,114 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
+export default [
+  {
     ignores: ["!**/.server", "!**/.client"],
-}, ...compat.extends("eslint:recommended"), {
+  },
+  ...compat.extends("eslint:recommended"),
+  {
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.commonjs,
-        },
+      globals: {
+        ...globals.browser,
+        ...globals.commonjs,
+      },
 
-        ecmaVersion: "latest",
-        sourceType: "module",
+      ecmaVersion: "latest",
+      sourceType: "module",
 
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
-            },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
+      },
     },
-}, ...fixupConfigRules(compat.extends(
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-)).map(config => ({
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      "plugin:react/recommended",
+      "plugin:react/jsx-runtime",
+      "plugin:react-hooks/recommended",
+      "plugin:jsx-a11y/recommended",
+    ),
+  ).map(config => ({
     ...config,
     files: ["**/*.{js,jsx,ts,tsx}"],
-})), {
+  })),
+  {
     files: ["**/*.{js,jsx,ts,tsx}"],
 
     plugins: {
-        react: fixupPluginRules(react),
-        "jsx-a11y": fixupPluginRules(jsxA11Y),
+      react: fixupPluginRules(react),
+      "jsx-a11y": fixupPluginRules(jsxA11Y),
     },
 
     settings: {
-        react: {
-            version: "detect",
+      react: {
+        version: "detect",
+      },
+
+      formComponents: ["Form"],
+
+      linkComponents: [
+        {
+          name: "Link",
+          linkAttribute: "to",
         },
-
-        formComponents: ["Form"],
-
-        linkComponents: [{
-            name: "Link",
-            linkAttribute: "to",
-        }, {
-            name: "NavLink",
-            linkAttribute: "to",
-        }],
-
-        "import/resolver": {
-            typescript: {},
+        {
+          name: "NavLink",
+          linkAttribute: "to",
         },
+      ],
+
+      "import/resolver": {
+        typescript: {},
+      },
     },
-}, ...fixupConfigRules(compat.extends(
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-)).map(config => ({
+  },
+  ...fixupConfigRules(
+    compat.extends("plugin:@typescript-eslint/recommended", "plugin:import/recommended", "plugin:import/typescript"),
+  ).map(config => ({
     ...config,
     files: ["**/*.{ts,tsx}"],
-})), {
+  })),
+  {
     files: ["**/*.{ts,tsx}"],
 
     plugins: {
-        "@typescript-eslint": fixupPluginRules(typescriptEslint),
-        import: fixupPluginRules(_import),
+      "@typescript-eslint": fixupPluginRules(typescriptEslint),
+      import: fixupPluginRules(_import),
     },
 
     languageOptions: {
-        parser: tsParser,
+      parser: tsParser,
     },
 
     settings: {
-        "import/internal-regex": "^~/",
+      "import/internal-regex": "^~/",
 
-        "import/resolver": {
-            node: {
-                extensions: [".ts", ".tsx"],
-            },
-
-            typescript: {
-                alwaysTryTypes: true,
-            },
+      "import/resolver": {
+        node: {
+          extensions: [".ts", ".tsx"],
         },
+
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
     },
-}, {
+  },
+  {
     files: ["**/.eslintrc.cjs"],
 
     languageOptions: {
-        globals: {
-            ...globals.node,
-        },
+      globals: {
+        ...globals.node,
+      },
     },
-}];
+  },
+];
